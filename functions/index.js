7796b94fbaccc7a9cf00fdac001abb1f6aca7caa
tomomichi <tomomichi.onishi@gmail.com>
2018-08-21 14:56:38 +0900

    split lib, delete pro

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
+const fs = require('fs');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+var serviceAccount = require('./cert.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
 
 console.log("hoge");
+fs.readFile('./ogp.html', 'utf8', function (err, templateHtml) {
+  if(err) { res.status(500).send(err); }
+  console.log("file read!");
+});
 
 
 /* Trigger: /sekki/xxxx  */
--- js/app.js
+++ js/app.js
@@ -3,29 +3,20 @@ $(function(){
   var current = sekki.current();
   setSekki(current);
 
+  // document.fonts.ready.then(function () {
+  //   html2canvas(document.body).then(function(canvas){
+  //     console.log("finished");
+  //     //imgタグのsrcの中に、html2canvasがレンダリングした画像を指定する。
+  //     var imgData = canvas.toDataURL();
+  //     console.log(imgData);
+  //     document.getElementById("result").src = imgData;
+  //   });
+  // });
+
+
   $(document).on('click', '#random', function(){
-    var today = getFormattedDate();
-    chrome.storage.local.get("count", function (value) {
-      var count = 0;
-      if(value['count'] && value['count'][today]) {
-        var count = value['count'][today];
-      }
-
-      if(count < 10) {
-        var data = {};
-        data[today] = count+1;
-        chrome.storage.local.set({'count': data}, function () {
-          var rand = sekki.random();
-          setSekki(rand);
-        });
-      }else {
-        $('.small.modal').modal('show');
-      }
-    });
-  });
-
-  $(document).on('click', ".close", function(){
-    $('.small.modal').modal('hide');
+    var rand = sekki.random();
+    setSekki(rand);
   });
 });
 
